775b4909d4be8bab3b13745ac2d5956d99356a0a
Nazlin <jemelinazlin@gmail.com>
2024-04-04 08:13:41 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+function returnsCustomerName() {
+    return customerName;
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newFavorite) {
+    bestCustomer = newFavorite;
+}
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'initial least favorite'; 
+    leastFavoriteCustomer = 'new least favorite'; 
+}
+
+module.exports = {
+    customerName,
+    returnsCustomerName,
+    upperCaseCustomerName,
+    setBestCustomer,
+    overwriteBestCustomer,
+    changeLeastFavoriteCustomer
+};
